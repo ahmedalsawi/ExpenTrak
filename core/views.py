1fb2986880a27e5958a23922ccc94243a5402a54
Ahmed Alsawi <ahmedalsawi2012@gmail.com>
2020-07-22 01:00:30 +0200

    Added jwt login and auth app

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets, filters, decorators
+from rest_framework.permissions import IsAuthenticated
 
 from . import models
 from . import serializers
@@ -9,3 +10,4 @@ from . import serializers
 class TransactionView(viewsets.ModelViewSet):
     queryset = models.Transaction.objects.all()
     serializer_class = serializers.TransactionSerializer
+    #permission_classes = (IsAuthenticated,)
