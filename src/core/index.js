cfe30fb77ebeae09d20735be82b6a4676d85d516
Ahmed Alsawi <ahmedalsawi2012@gmail.com>
2020-07-22 00:09:27 +0200

    Moved To django and DRF

--- src/core/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/transactions', require('./controllers/transaction.controller'));
-router.use('/accounts', require('./controllers/account.controller'));
-
-module.exports = router;
