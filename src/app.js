03ef5c2dd6b79763c7ddebb0cc51171d48370db1
Ahmed Alsawi <ahmedalsawi2012@gmail.com>
2020-07-21 11:47:22 +0200

    configure prettier and eslint

--- src/app.js
+++ src/app.js
@@ -11,4 +11,6 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
